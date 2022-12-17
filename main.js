4ab684da9d303fe1fa3e2f8a8f3da54f00db17c2
Tom Doder <tdoder89@gmail.com>
2022-12-17 14:46:12 -0600

    feat changes to radio buttons

---
+++ main.js
@@ -0,0 +1,4 @@
+var affirmationRadio = document.querySelector('.affirmation-radio')
+var mantraRadio = document.querySelector('.mantra-radio')
+var receiveMessageButton = document.querySelector('.receive-button')
+var messageBox = document.querySelector('.textbox2')
